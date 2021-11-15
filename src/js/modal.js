18af5edd42c6e105637ae8e1195f6a6e923466e0
Sandra Martini <sandrinhaamartini@gmail.com>
2021-11-15 10:09:00 -0300

    feat(modal): construída função anônima para mostrar o modal Preâmbulo 1B
    * Função é chamada ao clikar no botão "Fechar" do Preâmbulo 1A.

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,18 @@ $(document).ready(
 	function() {
 
 		$('#preambulo_1a').modal('show');
-		scrollTo(0,$S("main")[0].offsetTop)
+		scrollTo(0,$S("main")[0].offsetTop);
+
+	}
+
+	function () {
+
+	$('#fechar').onS('click' function () {
+
+		$('#preambulo_modos').modal('show');
+		scrollTo(0,$S("main")[0].offsetTop);
+
+	})
 
 	}
 )
