41d8d8d45256968824b608fd696dc8dbbef958de
Wisterleys <wister.sousa2@gmail.com>
2021-11-15 10:12:30 -0300

    test(modal.js): abertura do preambulo modos de jogo

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,19 +1,29 @@
-$(document).ready(
-	function() {
 
-		$('#preambulo_1a').modal('show');
-		scrollTo(0,$S("main")[0].offsetTop);
+function scrollS(){scrollTo(0,$S("main")[0].offsetTop);}
+function btnNovoJogo() {
 
-	}
+	$('#preambulo_1a').modal('show');
+	scrollS()
+	btnCarregarjogo()
 
-	function () {
+}
 
-	$('#fechar').onS('click' function () {
+function btnCarregarjogo() {
+
+	$S('#fechar').onS('click',function () {
 
 		$('#preambulo_modos').modal('show');
-		scrollTo(0,$S("main")[0].offsetTop);
+		scrollS()
+		
 
 	})
 
+}
+
+
+$(document).ready(
+	function(){
+		
+		btnNovoJogo()
 	}
 )
