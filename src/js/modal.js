152a2f62dfa127e0e5c8f92c601e02108dd7b88f
Sandra Martini <sandrinhaamartini@gmail.com>
2021-11-15 21:59:55 -0300

    feat(modal.js): refatorado a chamada dos modais
    * Comentei o código antigo;
    
    * A chamada dos modais em sequência (somente para desenvolvimento) foi feita usando os atributos data-bs-target e data-bs-toggle;
    
    * Foi mantida a chamada do primeiro modal com o carregamento da página.

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,21 @@
+function scrollS() {
 
-function scrollS(){scrollTo(0,$S("main")[0].offsetTop);}
+	scrollTo(0,$S("main")[0].offsetTop);
+}
+
+$(document).ready (
+
+	function () {
+
+	$('#modal_partidas').modal('show');
+
+	scrollS();
+
+	}
+
+)
+
+/*function scrollS(){scrollTo(0,$S("main")[0].offsetTop);}
 function btnNovoJogo() {
 
 	$('#preambulo_1a').modal('show');
@@ -14,7 +30,7 @@ function btnCarregarjogo() {
 
 		$('#preambulo_modos').modal('show');
 		scrollS()
-		
+
 
 	})
 
@@ -25,7 +41,7 @@ function open(params) {
 		case "Novo jogo":
 			$('#preambulo_2b').modal('show');
 			break;
-	
+
 		default:
 			break;
 	}
@@ -42,12 +58,15 @@ function toogle(el){
 
 $(document).ready(
 	function(){
-		
+
 		btnNovoJogo()
 		$S("#escolhe").onS("click",function(){
 			console.log("!")
 			open(this.value)
 			this.value= toogle(this)
 		})
+		$S("#fechar3").onS("click", function() {
+			$S("#modal_Pvl").modal('show');
+		})
 	}
-)
+)*/
